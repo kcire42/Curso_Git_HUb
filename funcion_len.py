9646aa8ce4ab2a095ca61d4e5d3ac0b8541b847a
Erick Carrillo <erick.carrillo@autoliv.com>
2021-07-22 11:04:55 -0500

    Se agrega rutina de contraseña y generador de palabras

--- funcion_len.py
+++ funcion_len.py
@@ -1,3 +1,6 @@
+import random
+
+
 def longitud_cadena():
     cadena = input(str("Ingrse la cadena"))
     j = 0
@@ -16,20 +19,37 @@ def pares_impar():
     
 
 def generador_contraseñas():
-    letras_minusculas = ("a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z")
-    letras_mayusculas = ("A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z")
-    caracteres_extraños = ("!","","#","$","%","&","/","(",")","=","?","'","¿","¡")
-    caracteres = (letras_minusculas, letras_mayusculas, caracteres_extraños)
-    print(caracteres)
+    letras_minusculas = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
+    letras_mayusculas = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    caracteres_extraños = ["!","","#","$","%","&","/","(",")","=","?"]
+    caracteres = letras_mayusculas + letras_minusculas + caracteres_extraños
+    ##print(caracteres)
+    contraseña = []
+    for i in range (10):
+        contraseña_caracter = random.choice(caracteres)
+        contraseña.append(contraseña_caracter)
+        ##print(contraseña_caracter)
+    ##print(contraseña)
+    contraseña = "".join(contraseña)    
+    print(f"Tu nueva contraseña: {contraseña}")
+
+def palabra():
+    nombre = str(input("Ingrese una palabra"))
+    cantidad = int(input("Ingrese el numero de veces que desea repetirla"))
+    j= 0
+    for i in range (cantidad):
+        j += 1
+        print(f"{j}- {nombre}")
 
 
 if __name__ == "__main__":
-    opcion = int(input("Ingrese el numero deseado"))
     print("""
     1- Longitud de Cadena
     2- Numero  par y impar
     3- Generador de contraseñas
     """)
+    opcion = int(input("Ingrese el numero deseado: "))
+
 
     if opcion == 1:
         longitud_cadena()
