93c2c0aafa29bbbe60a4144012ea1d45682f8347
Erick Carrillo <erick.carrillo@autoliv.com>
2021-07-26 21:41:39 -0500

    problema

--- funcion_len.py
+++ funcion_len.py
@@ -95,7 +95,7 @@ def interes ():
 
 if __name__ == "__main__":
     print("""
-    Este es el menu que hice 
+    Este es el menu que hico 
     Acabas de entrar al multiverso de la programacion bienvenido al universo 617 antes estabas en el 616
     1- Longitud de Cadena
     2- Numero  par y impar
